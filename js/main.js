195a12e0c357fa585dd2954717a33e463bb9e174
alberto <albertozanola1996@gmail.com>
2021-09-16 13:35:06 +0200

    Scaffolding

---
+++ js/main.js
@@ -0,0 +1,10 @@
+Vue.config.devtools = true;
+// Attraverso l'apposita API di Boolean:
+// (https://flynn.boolean.careers/exercises/api/random/mail)
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+const app = new Vue ({
+    el: "#root",
+    data: {},
+});
+// Bonus:
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
